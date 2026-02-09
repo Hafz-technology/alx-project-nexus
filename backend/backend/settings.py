06fd6bef4889ee566823ddd5eae1182f35fa9a9e
Khalil Hafz <khalilhafz@gmail.com>
2026-02-09 20:55:27 +0200

    updating backend setting file - setup api & product apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from datetime import timedelta
+from dotenv import load_dotenv
+
+
+
+load_dotenv() # Load environment variables from .env file
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +34,24 @@ SECRET_KEY = 'django-insecure-1)&ha3c!i*4hy0(xqnvlh1#-dgdqc!$1mrhw!o%ryvm#%2_v9a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+REST_FRAMEWORK = {
+    "DEFUALT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+
 
 
 # Application definition
@@ -37,6 +63,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api",
+    "products",
+    "rest_framework",
+    "corsheaders",
+    
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -47,6 +80,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    
+    
+    
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -115,3 +152,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
+
+
+
+
+
